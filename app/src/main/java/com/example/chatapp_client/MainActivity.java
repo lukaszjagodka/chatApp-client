ce6f9015d70ba8fd041fbe3ac87181b4923f415f
encorey <lukasz.jagodka@protonmail.com>
2020-12-28 17:16:30 +0100

    Initial commit

---
+++ app/src/main/java/com/example/chatapp_client/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.chatapp_client;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
