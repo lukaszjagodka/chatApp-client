50fc02fc18385f7b97c65f7ec16603bd07b1f3ed
encorey <lukasz.jagodka@protonmail.com>
2020-12-28 18:03:55 +0100

    added splashActivity

--- app/src/main/java/com/example/chatapp_client/MainActivity.java
+++ app/src/main/java/com/example/chatapp_client/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.chatapp_client;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Objects;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashActivity = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(splashActivity);
+            }
+        }, 3000);
     }
 }
---
+++ app/src/main/java/com/example/chatapp_client/SignupActivity.java
@@ -0,0 +1,4 @@
+package com.example.chatapp_client;
+
+public class SignupActivity {
+}
