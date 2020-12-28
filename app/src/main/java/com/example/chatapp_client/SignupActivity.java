59636e96847e4ae163274d5dc9268fae76b0aec3
encorey <lukasz.jagodka@protonmail.com>
2020-12-28 18:35:15 +0100

    added signupActivity with xml

--- app/src/main/java/com/example/chatapp_client/SignupActivity.java
+++ app/src/main/java/com/example/chatapp_client/SignupActivity.java
@@ -1,4 +1,32 @@
 package com.example.chatapp_client;
 
-public class SignupActivity {
+import android.view.View;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Objects;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SignupActivity.this, "Login", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        findViewById(R.id.signup).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SignupActivity.this, "Signup", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
