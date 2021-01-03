c3f7a24593a085ed4e132bb7546cce815202722b
encorey <lukasz.jagodka@protonmail.com>
2021-01-03 13:24:20 +0100

    added post /passwordchange route to RetrofitInterface

--- app/src/main/java/com/example/chatapp_client/retrofit/RetrofitInterface.java
+++ app/src/main/java/com/example/chatapp_client/retrofit/RetrofitInterface.java
@@ -3,6 +3,8 @@ package com.example.chatapp_client.retrofit;
 import com.example.chatapp_client.utils.LoginResult;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 import java.util.HashMap;
@@ -16,4 +18,8 @@ public interface RetrofitInterface {
 
   @POST("/rememberpassword")
   Call<Void> executeRememberPassword(@Body HashMap<String, String> map);
+
+  @Headers({ "Content-Type: application/json;charset=UTF-8"})
+  @POST("/passwordchange")
+  Call<Void> executePasswordChange(@Header("Authorization") String authToken, @Body HashMap<String, String> map);
 }
