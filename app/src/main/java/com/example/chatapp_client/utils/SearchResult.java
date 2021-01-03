ab64739f455bddfb00d2da1b41bd4c7adb03ad15
encorey <lukasz.jagodka@protonmail.com>
2021-01-03 15:57:08 +0100

    added SearchResult class

---
+++ app/src/main/java/com/example/chatapp_client/utils/SearchResult.java
@@ -0,0 +1,15 @@
+package com.example.chatapp_client.utils;
+
+import java.util.ArrayList;
+
+public class SearchResult {
+  private ArrayList<Object> findedUsers;
+
+  public ArrayList<Object> getFindedUsers() {
+    return findedUsers;
+  }
+
+  public void setFindedUsers(ArrayList<Object> findedUsers) {
+    this.findedUsers = findedUsers;
+  }
+}
