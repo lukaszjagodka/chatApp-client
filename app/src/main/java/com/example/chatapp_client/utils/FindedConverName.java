467ff1fe161fd6c6f88db5194bf388b01bbe15f1
encorey <lukasz.jagodka@protonmail.com>
2021-01-06 17:06:57 +0100

    added FindedConverName class for retrofit post route

---
+++ app/src/main/java/com/example/chatapp_client/utils/FindedConverName.java
@@ -0,0 +1,13 @@
+package com.example.chatapp_client.utils;
+
+public class FindedConverName {
+  public String getConversationName() {
+    return conversationName;
+  }
+
+  public void setConversationName(String conversationName) {
+    this.conversationName = conversationName;
+  }
+
+  private String conversationName;
+}
