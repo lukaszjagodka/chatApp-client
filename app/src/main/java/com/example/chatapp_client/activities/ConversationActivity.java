01668db639bb54a97233988f7ed7eabf00a6a3fd
encorey <lukasz.jagodka@protonmail.com>
2021-01-10 00:38:01 +0100

    initiateSocketConnection

--- app/src/main/java/com/example/chatapp_client/activities/ConversationActivity.java
+++ app/src/main/java/com/example/chatapp_client/activities/ConversationActivity.java
@@ -1,38 +1,56 @@
 package com.example.chatapp_client.activities;
 
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.recyclerview.widget.RecyclerView;
 import com.example.chatapp_client.R;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.WebSocket;
 
 public class ConversationActivity extends AppCompatActivity {
+    String name, convName;
+    int userId;
+    private WebSocket webSocket;
+    private String SERVER_PATH = "";
+    private EditText messageEdit;
+    private View sendBtn, pickImgBtn;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversation);
 
-        String name;
-        int userId;
-        String convName;
-        if (savedInstanceState == null) {
+
+//        if (savedInstanceState == null) {
             Bundle extras = getIntent().getExtras();
-            if(extras == null) {
-                userId = 0;
-                name = null;
-                convName = null;
-            } else {
+//            if(extras == null) {
+//                userId = 0;
+//                name = null;
+//                convName = null;
+//            } else {
                 userId = extras.getInt("userId");
                 name = extras.getString("name");
                 convName = extras.getString("conversationName");
-            }
-        } else {
-            userId = (Integer) savedInstanceState.getSerializable("userId");
-            name = (String) savedInstanceState.getSerializable("name");
-            convName = (String) savedInstanceState.getSerializable("conversationName");
-        }
+//            }
+//        } else {
+//            userId = (Integer) savedInstanceState.getSerializable("userId");
+//            name = (String) savedInstanceState.getSerializable("name");
+//            convName = (String) savedInstanceState.getSerializable("conversationName");
+//        }
         setTitle(name);
 //        TextView textView = findViewById(R.id.ccoz);
 //        textView.setText(convName);
+        initiateSocketConnection();
+    }
+
+    private void initiateSocketConnection() {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(SERVER_PATH).build();
+        webSocket = client.newWebSocket(request, )
     }
 }
