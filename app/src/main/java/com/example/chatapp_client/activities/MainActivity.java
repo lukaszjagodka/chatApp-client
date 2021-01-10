711791f01aa17e4288330316faded7109cbc2d84
encorey <lukasz.jagodka@protonmail.com>
2021-01-10 15:58:29 +0100

    External storage permission

--- app/src/main/java/com/example/chatapp_client/activities/MainActivity.java
+++ app/src/main/java/com/example/chatapp_client/activities/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.chatapp_client.activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Handler;
 import android.os.Looper;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
 import com.example.chatapp_client.R;
 
 import java.util.Objects;
@@ -15,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Objects.requireNonNull(getSupportActionBar()).hide();
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},10);
 
         new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
